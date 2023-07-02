db54af9e05f7e2c09b4b080c3e782cce1d7e0122
Leonel <leonelsanz15@gmail.com>
2023-07-02 19:11:10 -0300

    authenticate added

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,27 @@
 import express from "express";
 import cors from "cors";
+import axios from "axios";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
+dotenv.config();
+
 app.post("/authenticate", async (req,res) => {
     const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..." });
+
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            {username: username, secret: username, first_name: username},
+            { headers: { "Private-Key": process.env.CHAT_ENGINE_PRIVATE_KEY } }
+        )
+        return res.status(r.status).json(r.data)
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data)
+    }
 });
 
 app.listen(3001);
