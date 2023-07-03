384da7be7f626368f7fcf2024d769bac79ae9434
Leonel <leonelsanz15@gmail.com>
2023-07-03 17:44:21 -0300

    Base Setup

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,26 @@
 import express from "express";
 import cors from "cors";
-import axios from "axios";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
-app.use(express.json());
-app.use(cors({ origin: true }));
-
 dotenv.config();
 
-app.post("/authenticate", async (req,res) => {
-    const { username } = req.body;
+app.use(express.json());
+app.use(cors());
 
-    try {
-        const r = await axios.put(
-            'https://api.chatengine.io/users/',
-            {username: username, secret: username, first_name: username},
-            { headers: { "Private-Key": process.env.CHAT_ENGINE_PRIVATE_KEY } }
-        )
-        return res.status(r.status).json(r.data)
-    } catch (e) {
-        return res.status(e.response.status).json(e.response.data)
-    }
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successfully");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server Started on Port ${process.env.PORT}`);
 });
-
-app.listen(3001);
