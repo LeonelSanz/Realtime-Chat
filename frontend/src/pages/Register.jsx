e5e2cf601414ecd868ed632d432178ec6d10ffdf
Leonel <leonelsanz15@gmail.com>
2023-07-04 14:13:01 -0300

    register styles

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 
 function Register() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
   const handleSubmit = (event) => {
     event.preventDefault();
     alert("form");
   };
-  const handleChange = (event) => {
 
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
   };
+
   return (
     <>
       <FormContainer>
@@ -45,7 +54,7 @@ function Register() {
           />
           <button type="submit">Create User</button>
           <span>
-            already have an account ? <Link to="/login">Login</Link>
+            Already have an account? <Link to="/login">Login</Link>
           </span>
         </form>
       </FormContainer>
@@ -93,6 +102,29 @@ const FormContainer = styled.div`
       &:focus {
         border: 0.1rem solid #997af0;
         outline: none;
+      }
+    }
+    button {
+      background-color: #997af0;
+      color: white;
+      padding: 1rem 2rem;
+      border: none;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      transition: 0.5s ease-in-out;
+      &:hover {
+        background-color: #4e0eff;
+      }
+    }
+    span {
+      color: white;
+      a {
+        color: #4e0eff;
+        text-decoration: none;
+        font-weight: bold;
       }
     }
   }
