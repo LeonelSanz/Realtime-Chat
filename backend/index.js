abbbe1d7af3554984eefcd495e87af7d21277bf7
Leonel <leonelsanz15@gmail.com>
2023-07-05 15:29:59 -0300

    register functionality completed

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+require("dotenv").config();
 
 const app = express();
-dotenv.config();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL, {
