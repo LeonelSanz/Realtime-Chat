e879f6e2a8bd4ac3361d8a9b20411e10d1483cb3
Leonel <leonelsanz15@gmail.com>
2023-07-06 14:07:19 -0300

    Login Functionality finished

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.svg";
@@ -24,6 +24,12 @@ function Register() {
     draggable: true,
     theme: "dark",
   };
+
+  useEffect(() => {
+    if (localStorage.getItem("chat-app-user")) {
+      navigate('/')
+    }
+  }, [navigate]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
