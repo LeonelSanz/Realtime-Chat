f83225c63c9315d9b71bddb1d6f6119757ca1ed1
Leonel <leonelsanz15@gmail.com>
2023-07-12 15:29:24 -0300

    Chat Messages

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,63 +1,151 @@
-import React from 'react';
-import styled from 'styled-components';
-import Logout from './Logout';
-import ChatInput from './ChatInput';
-import Messages from './Messages';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Logout from "./Logout";
+import ChatInput from "./ChatInput";
+import Messages from "./Messages";
+import axios from "axios";
+import { getAllMessagesRoute, sendMessageRoute } from "../utils/APIRoutes";
 
 export default function ChatContainer({ currentChat }) {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await JSON.parse(
+                localStorage.getItem("chat-app-user")
+            );
+            const response = await axios.post(getAllMessagesRoute, {
+                from: data._id,
+                to: currentChat._id,
+            });
+            setMessages(response.data);
+        };
+
+        fetchData();
+    }, [currentChat]);
+
+    useEffect(() => {
+        const getCurrentChat = async () => {
+          if (currentChat) {
+            await JSON.parse(
+              localStorage.getItem("chat-app-user")
+            )._id;
+          }
+        };
+        getCurrentChat();
+      }, [currentChat]);
+
     const handleSendMsg = async (msg) => {
-        
+        const data = await JSON.parse(
+            localStorage.getItem("chat-app-user")
+        );
+        await axios.post(sendMessageRoute, {
+            from: data._id,
+            to: currentChat._id,
+            message: msg,
+        });
     };
 
     return (
         <>
-            {
-                currentChat && (
-                    <Container>
-                        <div className="chat-header">
-                            <div className="user-details">
-                                <div className="avatar">
-                                    <img
-                                        src={`data:image/svg+xml;base64,${currentChat.avatarImage}`}
-                                        alt="avatar"
-                                    />
-                                </div>
-                                <div className="username">
-                                    <h3>{currentChat.username}</h3>
-                                </div>
+            {currentChat && (
+                <Container>
+                    <div className="chat-header">
+                        <div className="user-details">
+                            <div className="avatar">
+                                <img
+                                    src={`data:image/svg+xml;base64,${currentChat.avatarImage}`}
+                                    alt="avatar"
+                                />
                             </div>
-                            <Logout />
+                            <div className="username">
+                                <h3>{currentChat.username}</h3>
+                            </div>
                         </div>
-                        <Messages />
-                        <ChatInput handleSendMsg={handleSendMsg} />
-                    </Container>
-                )
-            }
+                        <Logout />
+                    </div>
+                    <div className="chat-messages">
+                        {messages.map((message) => {
+                            return (
+                                <div>
+                                    <div
+                                        className={`message ${message.fromSelf ? "sended" : "recieved"
+                                            }`}
+                                    >
+                                        <div className="content">
+                                            <p>{message.message}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <ChatInput handleSendMsg={handleSendMsg} />
+                </Container>
+            )}
         </>
     );
-};
+}
 
 const Container = styled.div`
-    padding-top: 1rem;
-    .chat-header {
+  padding-top: 1rem;
+  display: grid;
+  grid-template-rows: 10% 78% 12%;
+  gap: 0.1rem;
+  overflow: hidden;
+  @media screen and (min-width: 720px) and (max-width: 1080px) {
+    grid-template-rows: 15% 70% 15%;
+}
+  .chat-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 2rem;
+    .user-details {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      .avatar {
+        img {
+          height: 3rem;
+        }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+      }
+    }
+  }
+  .chat-messages {
+    padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    overflow: auto;
+    .message {
         display: flex;
-        justify-content: space-between;
         align-items: center;
-        padding: 0 2rem;
-        .user-details {
-            display: flex;
-            align-items: center;
-            gap: 1rem;
-            .avatar {
-                img {
-                    height: 3rem;
-                }
-            }
-            .username {
-                h3 {
-                    color: white;
-                }
-            }
+        .content {
+            max-width: 40%;
+            overflow-wrap: break-word;
+            padding: 1rem;
+            font-size: 1.1rem;
+            border-radius: 1rem;
+            color: #d1d1d1;
         }
     }
+    .sended {
+        justify-content: flex-end;
+        .content {
+            background-color: #4f04ff21;
+        }
+    }
+    .recieved {
+        justify-content: flex-start;
+        .content {
+            background-color: #9900ff20;
+        }
+    }
+  }
 `;
