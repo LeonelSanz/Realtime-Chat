0809744a0f478bfbe1571e88122b40aed4e54319
Leonel <leonelsanz15@gmail.com>
2023-07-28 20:36:34 -0300

    vercel configuration frontend

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors:{
-    origin:"http://localhost:3000",
+    origin:"realtime-chat-olive.vercel.app",
     credentials: true
   }
 });
