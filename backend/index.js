cc797f2b610c1ff757fc66ef626c7ee7b8ac25a8
Leonel <leonelsanz15@gmail.com>
2023-07-29 15:35:39 -0300

    cors config 2

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const app = express();
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://realtime-chat-olive.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
