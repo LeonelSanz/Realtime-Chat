a8064ed9445997b9fe6122bf003105973edd4a45
Leonel <leonelsanz15@gmail.com>
2023-07-29 15:16:45 -0300

    update api url

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "realtime-chat-rho.vercel.app";
+export const host = "https://realtime-chat-api-six.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
