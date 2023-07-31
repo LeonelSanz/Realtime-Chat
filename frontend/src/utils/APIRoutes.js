16569079700dbf14bbfa7ac7352e0363d2280a00
Leonel <leonelsanz15@gmail.com>
2023-07-31 20:01:01 -0300

    change back to render

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://realtime-chat-api-six.vercel.app";
+export const host = "https://realtime-chat-backend-z8qv.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
