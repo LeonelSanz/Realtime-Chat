7318b1389d9cc94e618b9bacd1aa3428707a6064
Leonel <leonelsanz15@gmail.com>
2023-08-12 20:03:44 -0300

    fix url

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://realtime-chat-back-tvfp-dev.fl0.io/";
+export const host = "https://realtime-chat-back-tvfp-dev.fl0.io";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
